51e37ed86050c053bcb3e8cbb6a9728ee846b17f
pali <p.mikulaj@gmail.com>
2022-12-28 16:51:39 +0100

    bondaries set for players

--- foosball.js
+++ foosball.js
@@ -26,6 +26,9 @@ function preload() {
     this.load.image('playground', 'assets/playground.jpeg');
     this.load.image('player_blue', 'assets/player_blue.png');
     this.load.image('player_red', 'assets/player_red.png');
+
+
+    
 }
 function create() {   // playground
     var playground = this.add.image(400, 300, 'playground');
@@ -34,7 +37,7 @@ function create() {   // playground
 
     bluePlayers = createPlayers('player_blue', [[40, 300], [240, 200], [240, 400]], 90, this);
     redPlayers = createPlayers('player_red', [[760, 300], [560, 200], [560, 400]], -90, this);
-
+    
     keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
 }
@@ -51,22 +54,34 @@ function update() {
 
 function createPlayers(imageName, positions, rotation, object) {
 
-    players = object.physics.add.group();
+    var players = object.physics.add.group();
     for (var i = 0; i < positions.length; ++i) {
-        var player = object.add.sprite(positions[i][0], positions[i][1], imageName);
+        var player = object.physics.add.sprite(positions[i][0], positions[i][1], imageName);
         player.displayHeight = player.height / 5
         player.displayWidth = player.width / 5
         player.angle = player.angle + rotation;
         players.add(player);
+
     }
+    console.log(players.getChildren()[0].y);
     return players;
 }
 
 function handleKeyboardInput(cursorsUp, cursorsDown, players) {
-    if (cursorsUp.isDown) {
+    if (cursorsUp.isDown 
+        // https://phaser.discourse.group/t/how-do-i-make-a-group-collide-with-world-bounds/2448
+        // I tried to set boundaries by //player.body.setCollideWorldBounds(true);
+        // but it does set bonds to individual members of group - i.e. first player hits edge, but others
+        // are still moving until they hit edge too
+        // additional note: setColliderWorldBounds need to be set AFTER player is added to group
+        && players.getChildren()[1].y > 30
+        ) 
+        {
         players.setVelocityY(-160);
     }
-    else if (cursorsDown.isDown) {
+    else if (cursorsDown.isDown
+        && players.getChildren()[2].y < 570
+        ) {
         players.setVelocityY(160);
     }
     else if (cursorsUp.isUp) {
